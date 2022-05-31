6082848fe78d186a9ec359c93520e832405dd78b
Luciano Victor <lucianocarvallho7@gmail.com>
2022-05-31 13:48:56 -0300

    feat: update section Movies and styles

--- src/components/Movies/index.tsx
+++ src/components/Movies/index.tsx
@@ -1,22 +1,92 @@
-import movies from 'data/movies.json'
-
 import styles from './styles.module.scss'
 
 export const Movies = () => (
-  <section className={styles.container}>
+  <aside className={styles.movies}>
     <h2>Movies</h2>
-    <div className={styles.movies}>
-      {movies.map(movies => (
-        <div className={styles.movie} key={movies.name}>
-          <span>
-            <img src={movies.image} alt={`imagens dos filmes ${movies.name}`} />
-          </span>
-          <div className={styles.movie__description}>
-            <strong>{movies.name}</strong>
-            <p>{movies.category}</p>
-          </div>
-        </div>
-      ))}
+    <div>
+      <ul>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+        <li>
+          <span>Kids</span>
+          <strong>Spirit Untamed</strong>
+
+          <button type="button">
+            <img src="icons/player-video.svg" alt="Icon play button" />
+          </button>
+        </li>
+      </ul>
     </div>
-  </section>
+  </aside>
 )
